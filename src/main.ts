72223e7e24605d89f3f9e923b741eb465093834b
Luis Contreras <lockyalejandro5078@gmail.com>
2025-07-23 21:18:06 -0600

    Agregando Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuración Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API de Usuario')
+    .setDescription('Documentación del CRUD de usuarios')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Estará disponible en /api
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
